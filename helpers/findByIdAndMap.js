71fede8390279834d8bd2f93f6782214e7b0f505
Alina <alirei@gmail.com>
2021-09-01 15:16:30 +0200

    minor adjustments to server files

--- helpers/findByIdAndMap.js
+++ helpers/findByIdAndMap.js
@@ -1,14 +1,17 @@
 const axios = require("axios").default;
 
 
-exports.findByIdAndMap = (array, res, rapidApiHeaders) => {
+exports.findByIdAndMap = (array, res) => {
 
     const moviesInfo = array.map(async item => {
 
         let options = {
             method: 'GET',
             url: `https://data-imdb1.p.rapidapi.com/movie/id/${item.imdb_id}/`,
-            headers: rapidApiHeaders
+            headers: {
+                'x-rapidapi-host': 'data-imdb1.p.rapidapi.com',
+                'x-rapidapi-key': process.env.RAPID_API_KEY
+            }
         }
 
         return await axios.request(options)
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -7,40 +7,41 @@ const { upcomingMovies, moviesByUserGenre, moviesByTitle, moviesByDirector, movi
 // **************************************
 // GET upcoming movies
 router.get("/upcoming", upcomingMovies)
-// check: http://localhost:5000/movie/upcoming
+// check: http://localhost:5000/api/movie/upcoming
 
 // GET top rated movies
 router.get("/toprated", topRatedMovies)
-// check: http://localhost:5000/movie/toprated
+// check: http://localhost:5000/api/movie/toprated
 
-// GET movies by genre and by user id
-router.get("/byGenre/:id", moviesByUserGenre)
+
+// GET movies by genre and by user id  - in process
+// router.get("/byGenre/:id", moviesByUserGenre)
 
 
 // FOR SEARCH:
 // **************************************
 // GET movies by title
 router.get("/title/:title", moviesByTitle)
-// check: http://localhost:5000/movie/title/london
+// check: http://localhost:5000/api/movie/title/london
 
 // GET movies by director
 router.get("/director/:director", moviesByDirector)
 
 // GET movies by genre
 router.get("/genre/:genre", moviesByGenre)
-// check: http://localhost:5000/movie/genre/comedy
+// check: http://localhost:5000/api/movie/genre/comedy
 
 
 // GET movies by year
 router.get("/year/:year", moviesByYear)
-// check: http://localhost:5000/movie/year/1999
+// check: http://localhost:5000/api/movie/year/1999
 
 
 
 // FOR INDIVIDUAL MOVIE:
 // **************************************
 router.get("/oneMovieById/:imdbId", movieById)
-// check: http://localhost:5000/movie/oneMovieById/tt0451279
+// check: http://localhost:5000/api/movie/oneMovieById/tt0451279
 
 
 // General search by title/year/genre/director
