a4326892a60414f661eef7963b10811316dde0fc
Luiza <luizafcampos@gmail.com>
2021-09-01 10:37:32 +0200

    testing

--- server/models/Movie.js
+++ server/models/Movie.js
@@ -1,9 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 // Creating Movie schema
-const MovieSchema = new Schema({
-
-})
+const MovieSchema = new Schema({});
 
 const Movie = model("Movie", MovieSchema);
 
