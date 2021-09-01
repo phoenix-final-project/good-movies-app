f13a4a9af6ecb9d6680d02cd1779df6c2c91fe2e
Victoria <victoria.zakhozhay@gmail.com>
2021-09-01 13:45:11 +0200

    local host in env

--- client/src/util/APIinstance.js
+++ client/src/util/APIinstance.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 const axiosApiInstance = axios.create();
 
-axiosApiInstance.defaults.baseURL = 'http://localhost:3000';
+axiosApiInstance.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 axiosApiInstance.defaults.headers.post['Content-Type'] = 'application/json';
-
 
 export default axiosApiInstance;
