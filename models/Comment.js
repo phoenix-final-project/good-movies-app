145bca1909146ef423ab1cecd6c3b634cfb43372
Victoria <victoria.zakhozhay@gmail.com>
2021-09-02 14:36:18 +0200

    all schemas

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ const { Schema, model } = require('mongoose');
 const CommentSchema = new Schema({
 	user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 	movieId: { type: String, required: true }, // for imdb id received from api
-	comment: { type: String, required: true },
+	comment: { type: String, required: [true, 'Please, leave your comment'] },
 	date: { type: Date, default: Date.now, required: true },
 	deleted: { type: Boolean, default: false },
 });
--- models/User.js
+++ models/User.js
@@ -1,12 +1,20 @@
+const Genres = require('./Genre');
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
-	username: { type: String, required: true, unique: true },
-	firstname: { type: String, required: true },
+	username: { type: String, required: [true, 'Please, provide a username'], unique: [true, 'This username already exists'] },
+	firstname: { type: String, required: [true, 'Please, provide your first name'] },
 	lastname: { type: String },
-	password: { type: String, required: true },
-	email: { type: String },
-	favoriteGenres: [{ genre: { type: Schema.Types.ObjectId, ref: 'Genre' } }],
+	password: { type: String, required: [true, 'Please, provide a password'], max: [5, 'The password must me at least 5 characters'] },
+	email: { type: String, required: [true, 'Please, provide your email address'] },
+	favoriteGenres: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: 'User',
+			min: [3, 'Please, choose your three favorite movie genres'],
+			max: [3, 'Please, choose your three favorite movie genres'],
+		},
+	],
 	registerDate: { type: Date, default: Date.now },
 	friends: [{ user: { type: Schema.Types.ObjectId, ref: 'User' }, date: { type: Date, default: Date.now }, deleted: { type: Boolean, default: false } }],
 	deleted: { type: Boolean, default: false }, // to delete the user
