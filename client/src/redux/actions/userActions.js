168d4057ba289e016196650fecfb0b5803114e69
Alex-Ksairi <alexanderksairi@web.de>
2021-09-06 13:50:36 +0200

    user reducer

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,5 +1,22 @@
-export const registerUser = () => {};
 
-export const loginUser = () => {};
+// user actions --> constance
+import { ActionTypes } from "../constants/action-types";
 
-export const logoutUser = () => {};
+export const registerUser = () => {
+    return {
+        type: ActionTypes.REGISTER_USER,
+    };
+};
+
+export const loginUser = (user, token) => {
+    return {
+        type: ActionTypes.LOGIN_USER,
+        payload: { user, token }
+    };
+};
+
+export const logoutUser = () => {
+    return {
+        type: ActionTypes.LOGOUT_USER,
+    };
+};
