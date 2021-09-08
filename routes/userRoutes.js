5285377cd432ba368c0829a1d162e0717a68629d
Alina <alirei@gmail.com>
2021-09-08 14:17:26 +0200

    changes to userControllers+routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,38 +3,43 @@ const router = express.Router();
 const passport = require("passport");
 
 const { validateUser, sanitizeUser } = require('../middleware/userValidation');
-const { registerUser, loginUser, updateUser, deleteUser, getUserByUsername, addFriend, findUserByAnyName, getFriendsOfUser } = require('../controllers/userControllers');
+const { registerUser, loginUser, updateUser, deleteUser, getUserByUsername, addFriend, findUserByAnyName, getFriendsOfUser, deleteFriend } = require('../controllers/userControllers');
 
 
-// register a new user
+// REGISTER A NEW USER
 router.post('/register', validateUser, sanitizeUser, registerUser);
 
-// login a user
+// LOGIN A USER
 router.post('/login', loginUser);
 
-// update a user - protected route - a user needs to be logged in to perform an action
+// UPDATE A USER- protected route - a user needs to be logged in to perform an action
 router.put('/update/:username', passport.authenticate("jwt", { session: false }), validateUser, sanitizeUser, updateUser);
 // router.put('/update/:username', validateUser, sanitizeUser, updateUser);
 
-// delete a user - PUT method - protected route - a user needs to be logged in to perform an action
+// DELETE A USER- PUT method - protected route - a user needs to be logged in to perform an action
 router.put('/delete', passport.authenticate("jwt", { session: false }), deleteUser);
 
-// get user by username
-router.get("/username/:username", getUserByUsername)
+// FIND **ONE** USER BY USERNAME - an object
+router.get('/username/:username', getUserByUsername)
 // check: http://localhost:5000/api/user/username/santa
 
-// get / find user(s) by first name / last name / username
-router.get("/find/:name", findUserByAnyName)
+// FIND USERS BY **ANY** NAME (username, firstname, lastname) - an array of objects
+router.get('/find/:name', findUserByAnyName)
 // check: http://localhost:5000/api/user/find/santa
 
 
+// ********************************************************
 // USER + FRIENDS
-// get friends of user(username of a user is used to make a request)
-router.get("/friends/:username", getFriendsOfUser)
+// GET ALL FRIENDS OF A USER (username of a user is used to make a request)
+router.get('/friends/:username', getFriendsOfUser)
 // check: http://localhost:5000/api/user/friends/santa
 
-// add a friend to a user - protected route - a user needs to be logged in to perform an action
-router.put("/friends/add", passport.authenticate("jwt", { session: false }), addFriend)
+// ADD A NEW FRIEND TO A USER - protected route - a user needs to be logged in to perform an action
+router.put('/friends/add', passport.authenticate("jwt", { session: false }), addFriend)
+
+// DELETE A FRIEND FROM A USER  - PUT method to edit the user - protected route - a user needs to be logged in to perform an action
+router.delete('/friends/delete', passport.authenticate("jwt", { session: false }), deleteFriend);
+
 
 
 module.exports = router;
