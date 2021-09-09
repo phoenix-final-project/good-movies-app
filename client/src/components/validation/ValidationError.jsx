7ef139c7b4589023fce07e9a3dfb1589e00e5d85
Alex-Ksairi <alexanderksairi@web.de>
2021-09-09 15:30:37 +0200

    registeration login done

--- client/src/components/validation/ValidationError.jsx
+++ client/src/components/validation/ValidationError.jsx
@@ -5,50 +5,27 @@ function ValidationErrorRegistration(values) {
     let errors = {};
 
     if (!values.username.trim()) {
-        errors.username = 'Enter a unique username';
+        errors.username = 'Username is required';
     }
 
     if (!values.firstname.trim()) {
-        errors.firstname = 'Enter your first name';
-    }
+        errors.firstname = 'firstname is required';
+    } 
 
     if (!values.lastname.trim()) {
-        errors.lastname = 'Enter your lastname';
-    }
+        errors.lastname = 'lastname is required';
+    } 
 
     if (!values.email) {
-        errors.email = 'Enter a valid email address.';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
-        errors.email = 'Email address is invalid!';
-    }
+        errors.email = 'email address is required';
+    } 
 
     if (!values.password) {
-        errors.password = 'Enter a password'
-    } else if (values.password.length < 5) {
-        errors.password = 'A strong password must have at least five characters or more'
-    }
+        errors.password = 'password is required'
+    } 
 
     return errors;
 };
 
-function ValidationErrorLogin(values) {
-    let errors = {};
-    if (!values.username) {
-        errors.username = 'Login with your existing username';
-    } else if (values.username !== values.username) {
-        errors.username = 'Email addresses don\'t match';
-    }
-
-    if (!values.password) {
-        errors.password = 'Login with your existing password'
-    } else if (values.password.length < 5) {
-        errors.password = 'A strong password must have at least five characters or more'
-    } else if (values.password !== values.password) {
-        errors.password = 'Passwords don\'t match'
-    }
-
-    return errors
-}
-
-export { ValidationErrorRegistration, ValidationErrorLogin };
+export default ValidationErrorRegistration;
 
