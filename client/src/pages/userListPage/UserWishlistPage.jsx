effab7b48115696983180654bb2a8477aff216c1
Victoria <victoria.zakhozhay@gmail.com>
2021-09-10 19:38:58 +0200

    styling of wishlist

--- client/src/pages/userListPage/UserWishlistPage.jsx
+++ client/src/pages/userListPage/UserWishlistPage.jsx
@@ -58,7 +58,7 @@ export default function UserWishlistPage() {
     return (
         <div className='movie-list-container'>
             <div className="movie-list-heading">
-                <h2>Want to Watch ({numOfMovies})</h2>
+                <h1>Want to Watch ({numOfMovies})</h1>
             </div>
 
             <div className='movie-list'>
@@ -73,21 +73,26 @@ export default function UserWishlistPage() {
                     <section>
                         <p>Title: <span>{movie.title}</span></p>
                         <p>Year: <span>{movie.year}</span></p>
-                        <p>Length: <span>{movie.movie_length}</span></p>
-                        <p>Rating: {movie.rating}</p>
+                        { movie.movie_length !== 0 && <p>Length: <span>{movie.movie_length}</span></p>}
+                        {movie.rating !== 0 && <p>Rating: <span>{movie.rating}</span></p>}
                     </section>
 
                     <section>
-                        <h4>Plot</h4>
-                        <p>{movie.plot}</p>
-
-                        {movie.gen.map(gen => <span>{gen.genre}</span>)}
+                        <div>
+                            <h4>Plot</h4>
+                            <p>{movie.plot}</p>
+                        </div>
+                        <div>
+                            {movie.gen.map(gen => <span>{gen.genre}</span>)}
+                        </div>
                     </section>
 
                     <section>
                         <button onClick={() => deleteMovie(movie.imdb_id)}>Delete</button>
                         
                         <button onClick={() => addMovieToWatched(movie)}>Watched</button>
+
+                        <button>Favorite</button>
                     </section>
                 </div>
             ))}
