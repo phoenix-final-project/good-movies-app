62e2bbddcfd5f61ec36dff90d926095feb45d269
Victoria <victoria.zakhozhay@gmail.com>
2021-09-12 11:29:48 +0200

    lists - get movie, if a movie is NOT in Redis

--- helpers/listsHelpers.js
+++ helpers/listsHelpers.js
@@ -1,18 +1,30 @@
 const { redisClient } = require('../redis-server');
+const findMovieById = require('../helpers/findMovieById');
 
 exports.getListFromCache = async (schema, userId) => {
 	const idMoviesFromList = await schema.find({ user: userId }).sort({
 		date: -1,
 	});
 
+	// go thru all movieIds of this user, return an array of movie objects
 	const moviesFromList = idMoviesFromList.map(async item => {
 		const { movieId } = item;
+		let foundMovie;
 
+		// find movie in Redis. If movie is not there, got to the external API
 		const movie = await redisClient.get(movieId);
-		return JSON.parse(movie);
+
+		if (movie) {
+			foundMovie = JSON.parse(movie);
+		} else {
+			const movieFromApi = await findMovieById(movieId);
+			foundMovie = Object.values(movieFromApi)[0];
+		}
+		return foundMovie;
 	});
 
 	return Promise.all(moviesFromList).then(data => {
+		console.log(data);
 		return { numOfMovies: data.length, data };
 	});
 };
