d3cf1d8b5fb9083a9175d9593c076fc1e03fdafc
Victoria <victoria.zakhozhay@gmail.com>
2021-09-12 12:56:59 +0200

    added ability to add a movie from modal to both wish- and watched lists

--- helpers/listsHelpers.js
+++ helpers/listsHelpers.js
@@ -1,6 +1,14 @@
 const { redisClient } = require('../redis-server');
 const findMovieById = require('../helpers/findMovieById');
 
+/**
+ *
+ * Returns a list of movie objects, received either from cache or from external API
+ *
+ * @param {*} schema
+ * @param {*} userId
+ */
+
 exports.getListFromCache = async (schema, userId) => {
 	const idMoviesFromList = await schema.find({ user: userId }).sort({
 		date: -1,
@@ -28,4 +36,41 @@ exports.getListFromCache = async (schema, userId) => {
 		console.log(data);
 		return { numOfMovies: data.length, data };
 	});
+};
+
+/**
+ * Checks if the movie is in the second list. If yes, then deletes it. After that, add the movie to the desired list.
+ * Returns message and the movie that was added to the desired list
+ * @param {*} firstList - mongoose Schema
+ * @param {*} secondList - mongoose Schema
+ * @param {*} userId - String
+ * @param {*} imdb_id - String
+ * @param {*} movie - Object
+ * @param {*} nameOfList - String
+ *
+ */
+exports.addMovieToList = async (firstList, secondList, userId, imdb_id, movie, nameOfList) => {
+	// STEP 1. If movie already in firstList, throw error
+	const ifMovieInList = await firstList.find({ user: userId, movieId: imdb_id });
+	if (ifMovieInList.length !== 0) throw { message: `This movie is already in the ${nameOfList}` };
+
+	// STEP 2. If movie in secondList list, delete from there.
+	const movieToDelete = await secondList.findOne({ user: userId, movieId: imdb_id });
+
+	if (movieToDelete) {
+		const deletedMovie = await movieToDelete.deleteOne();
+		console.log(deletedMovie);
+	} else {
+		await redisClient.set(imdb_id, JSON.stringify(movie));
+	}
+
+	// STEP 3. Add to firstList in DB
+	const movieToAdd = new firstList({
+		user: userId,
+		movieId: imdb_id,
+	});
+
+	await movieToAdd.save();
+
+	return { message: `Movie added to the ${nameOfList}`, movie };
 };
