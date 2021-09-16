81af7c8abff40a99868b9dd21608b4052bf6ec56
Alex-Ksairi <alexanderksairi@web.de>
2021-09-16 09:58:28 +0200

    go to top button, app description and styling error page

--- client/src/components/banner/Banner.jsx
+++ client/src/components/banner/Banner.jsx
@@ -6,47 +6,17 @@ import { Link } from "react-router-dom";
 import TextBanner from '../text-banner/TextBanner';
 
 // importing data banner
-import { changeBackgroundImage, textData } from "./DataBanner";
+import { changeBackgroundImage } from "./DataBanner";
 
 import './Banner.scss';
 
-// let intervalID;
-
 export default function Banner() {
-    // const [index, setIndex] = React.useState(0);
-    // // const [indexText, setIndexText] = React.useState(0);
-
-    // React.useEffect(() => {
-    //     // window.scrollTop = 0;
-    //     indexFunction();
-    //     return () => clearInterval(intervalID);
-    // }, [index]); // , [index]
-
-    // const indexFunction = () => {
-    //     intervalID = setInterval(() => {
-    //         setIndex(index => {
-    //             index = index + 1;
-    //             if (index === 3) {
-    //                 index = 0
-    //             }
-    //             return index;
-    //         });
-
-    //         // setIndexText(indexText => {
-    //         //     indexText = indexText + 1;
-    //         //     if (indexText === 3) {
-    //         //         indexText = 0
-    //         //     }
-    //         //     return indexText;
-    //         // });
-    //     }, 6000);
-    // }
-
+   
     return (
         <React.Fragment>
             <div className="banner">
                 <img className='img' src={changeBackgroundImage[1]} alt="cinema" />
-                <TextBanner title='Hello World' paragraph={textData[0]}/>
+                <TextBanner />
                 <div className="button-container">
                     <div className="discover-button">
                         <h3 className='check-h3'>Want first to discover the app?</h3>
