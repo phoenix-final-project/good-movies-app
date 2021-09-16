5a3219ac6416607f6a73aa8e0ce79f2fcaeadc6a
Victoria <victoria.zakhozhay@gmail.com>
2021-09-16 13:20:50 +0200

    redis for top rated movies

--- helpers/getDataRedisOrApi.js
+++ helpers/getDataRedisOrApi.js
@@ -3,25 +3,25 @@ const axios = require('axios').default;
 
 const getDataRedisOrApi = async (key, options) => {
 	try {
-		let foundTitles;
+		let foundData;
 
 		// 1. go to Redis and get data from cache
 		let result = await redisClient.get(key);
 
 		// 2. if in redis, give results, in not - fetch from api and store in Redis
 		if (result) {
-			foundTitles = JSON.parse(result);
+			foundData = JSON.parse(result);
 			console.log('REDIS');
 		} else {
 			result = await axios.request(options);
-			foundTitles = Object.values(result.data)[0];
+			foundData = Object.values(result.data)[0];
 			console.log('API');
 
 			// store in redis
-			await redisClient.setex(key, 1800, JSON.stringify(foundTitles));
+			await redisClient.setex(key, 1800, JSON.stringify(foundData));
 		}
-		// console.log(foundTitles);
-		return foundTitles;
+		// console.log(foundData);
+		return foundData;
 	} catch (error) {
 		return error.message;
 	}
