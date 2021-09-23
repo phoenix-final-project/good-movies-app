4f229476bc24f620d72588785ba7e4acea9d337d
Victoria <victoria.zakhozhay@gmail.com>
2021-09-23 16:38:11 +0200

    style of notification list

--- client/src/components/navigation/PrivateNavigation.js
+++ client/src/components/navigation/PrivateNavigation.js
@@ -1,44 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import NavBanner from '../navBanner/NavBanner';
-import useNotification from '../../hooks/useNotification';
-import axios from '../../util/APIinstance';
+import Notification from './Notification';
 
 // styling
 import './NavBar.scss';
 
 export default function PrivateNavigation() {
-	const [numOfNewNotifications, newNotifications, setNumOfNewNotifications] = useNotification();
-	const [allNotifications, setAllNotifications] = useState([]);
-	const [isDropdownMenuClicked, setIsDropdownMenuClicked] = useState(true);
-
 	const handleLogout = () => {
 		window.localStorage.clear();
 		window.location.href = '/';
-	};
-
-	const readNotifications = async () => {
-		// open-close drop-down list
-		setIsDropdownMenuClicked(!isDropdownMenuClicked);
-
-		// set new notifications as read (number of new notifications = 0)
-		if (numOfNewNotifications !== 0) {
-			const notificationsId = newNotifications.map(notification => notification._id);
-
-			notificationsId.forEach(async notificationId => {
-				try {
-					await axios.put(`/api/notification/set-to-read/${notificationId}`);
-
-					setTimeout(() => setNumOfNewNotifications(0), 700);
-				} catch (error) {
-					console.error(error.message);
-				}
-			});
-		}
-
-		// get a list of all notifications
-		const response = await axios.get(`/api/notification/all/${window.localStorage.getItem('user_id')}`);
-		setAllNotifications(response.data);
 	};
 
 	return (
@@ -60,36 +31,7 @@ export default function PrivateNavigation() {
 				<div className='tour'>PROFILE</div>
 			</NavLink>
 
-			<div className='notification' onClick={() => readNotifications()}>
-				<div className='notification-content'>
-					<i className={isDropdownMenuClicked ? 'far fa-bell' : 'far fa-bell clicked-far'}></i>
-					{numOfNewNotifications != 0 && <span>{numOfNewNotifications}</span>}
-				</div>
-				<div className={isDropdownMenuClicked ? 'dropdown-content' : 'dropdown-content-click'}>
-					<div className='dropdown-menu-text'>
-						{allNotifications.map((item, index) => (
-							// <p key={index}>{item.friend.username}</p>
-							<div key={index}>
-								<div>
-									<p>{item.friend.avatar}</p>
-									<p>{item.friend.username}</p>
-								</div>
-
-								<div>
-									<p>
-										{item.friend.username} wants to watch {item.movie.title} with you
-									</p>
-								</div>
-
-								<div>
-									<img src={item.movie.image} alt={item.movie.title} />
-								</div>
-							</div>
-						))}
-					</div>
-				</div>
-			</div>
-
+			<Notification />
 			<div>
 				<button className='logout-btn' title='Logout' onClick={handleLogout}>
 					logout
