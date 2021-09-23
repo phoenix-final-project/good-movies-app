c0986f9aa6514f2f563627980af760262805b7e3
Victoria <victoria.zakhozhay@gmail.com>
2021-09-23 13:59:37 +0200

    receive all the needed data on all notifications of a current user

--- controllers/notificationControllers.js
+++ controllers/notificationControllers.js
@@ -1,4 +1,6 @@
 const Notification = require('../models/Notification');
+const findMovieById = require('../helpers/findMovieById');
+const findByIdAndMap = require('../helpers/findByIdAndMap');
 
 exports.createNotification = async (req, res) => {
 	const { user1, user2, movieId } = req.body;
@@ -26,8 +28,36 @@ exports.getUserNotifications = async (req, res) => {
 	const { userId } = req.params;
 
 	try {
-		const userNotification = await Notification.find({ user2: userId, readStatus: false });
-		res.status(200).json({ numOfNew: userNotification.length, data: userNotification });
+		// get the number of new notifications
+		const numberOfNewNotifications = await Notification.count({ user2: userId, readStatus: false });
+
+		// get all notifications of a current user
+		const userNotificationsAll = await Notification.find({ user2: userId }).populate('user1');
+
+		// get the needed data of a friend and of a movie of all current user's notifications
+		const notificationDataRaw = userNotificationsAll.map(async notification => {
+			const userData = {
+				id: notification.user1._id,
+				username: notification.user1.username,
+				avatar: notification.user1.avatar,
+			};
+
+			const response = await findMovieById(notification.movieId);
+			const getMovieData = Object.values(response)[0];
+
+			const movieData = {
+				imdb_id: getMovieData.imdb_id,
+				title: getMovieData.title,
+				image: getMovieData.image_url,
+			};
+
+			return { friend: userData, movie: movieData };
+		});
+
+		// wait until receive all data
+		const notificationData = await Promise.all(notificationDataRaw);
+
+		res.status(200).json({ numOfNew: numberOfNewNotifications, notificationData });
 	} catch (error) {
 		res.status(400).json({ error: error.message });
 	}
