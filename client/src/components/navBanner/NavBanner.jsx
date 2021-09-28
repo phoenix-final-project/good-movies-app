733a33659e2acca06c3dbc934741e94fea53f18e
Alina <alirei@gmail.com>
2021-09-28 16:09:11 +0200

    working on responsiveness

--- client/src/components/navBanner/NavBanner.jsx
+++ client/src/components/navBanner/NavBanner.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 
 // logo
 import myLogo from '../../icons/CINEFILO.png';
+import Notification from '../navigation/Notification';
 
 // styling
 import './NavBanner.scss';
@@ -14,24 +15,38 @@ export default function NavBanner({ children }) {
 
 	return (
 		<React.Fragment>
-			<nav className='nav' id={isOpen ? 'radius' : ''}>
+			<nav className='nav'>
+
 				<div className='header-container'>
+
+					{window.localStorage.getItem("username") ?
+						<div className="tablet-notification" >
+							<Notification />
+						</div>
+						:
+						null
+					}
+
 					<Link to='/' className='link-logo' title='Home page'>
 						<div className='logo'>
 							<img className='img-logo' src={myLogo} alt='logo' />
 						</div>
 					</Link>
+
 					<div className='container-button' id={isOpen ? 'hidden' : ''} onClick={() => setIsOpen(!isOpen)}>
 						{children}
 					</div>
 
+
 					<div className='nav-close' id={isOpen ? 'hidden' : ''} onClick={() => setIsOpen(!isOpen)}>
 						✕
 					</div>
+
 					<div className='nav-toggle' onClick={() => setIsOpen(!isOpen)}>
 						<i className='fas fa-bars' id={isOpen ? '' : 'hidden'}></i>
 					</div>
 				</div>
+
 			</nav>
 		</React.Fragment>
 	);
